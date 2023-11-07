d33802cfc2964a39c70caa6544b4052c87bb7dd0
eroudini <eroudini.abdillah@yahoo.com>
2023-11-07 16:16:16 +0100

    😏

--- src/components/Honors.js
+++ src/components/Honors.js
@@ -5,8 +5,8 @@ import hr from "../assets/curve-hr.svg"
 export default function Honors(){
     return (
         <div id="honors" className="mt-4 text-white">
-            <h1 className="text-2xl font-bold">Honors & Awards</h1>
-            <p className="font-light text-gray-400">Here are some of my honors and awards</p>
+            <h1 className="text-2xl font-bold">Projets</h1>
+            <p className="font-light text-gray-400">Quelqu'uns de mes projets</p>
 
             <div className="flex flex-col md:flex-row mt-4 gap-5">
                 <HonorCard name="Top 10 SELEKNAS ASEAN Skills Competition ITNSA 2022" issued="KEMENAKER RI" desc="Linux Server, Windows Server, Linux Routing, Cisco Troubleshooting, Cisco Network Analysis, Network and System Programmabillity, Containerized Service." />
