4249cac6d8d17275ee0d41d268028411c8650594
eroudini <eroudini.abdillah@yahoo.com>
2023-11-07 16:41:13 +0100

    🙂

--- src/components/Certs.js
+++ src/components/Certs.js
@@ -2,7 +2,6 @@ import CertCard from "./CertCard.js"
 
 import cap1 from "../assets/certs/cap1.png"
 import hr from "../assets/curve-hr.svg"
-import adinusa from "../assets/certs/adinusa.png"
 
 
 // import 
@@ -11,12 +10,12 @@ export default function Certs(){
     return (
         <div id="certs" className="mt-4 text-white">
             <h1 className="text-2xl font-bold">Projets</h1>
-            <p className="font-light text-gray-400">Quelqu'uns de mes projets</p>
+            <p className="font-light text-gray-400">Voci quelques projets réalisé</p>
 
             {/* <div className="flex flex-col md:flex-row flex-wrap mt-4 gap-5"> */}
             <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
                 <CertCard name="Premier Projet, un site web statique juste en HTML et CSS" img={cap1} date="Juin 2023" />
-                
+
                 {/* <CertCard name="Bootcamp Btech Academy Cloud Engineer" img={adinusa} issued="Adinusa" date="Jul 2022" /> */}
 
 
