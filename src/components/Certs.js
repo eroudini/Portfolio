0c1987a9eb4d0ee19ac00c6ea3c464256af715e5
eroudini <eroudini.abdillah@yahoo.com>
2023-12-01 16:39:52 +0100

    nc

--- src/components/Certs.js
+++ src/components/Certs.js
@@ -1,6 +1,7 @@
 import CertCard from "./CertCard.js"
 
 import cap1 from "../assets/certs/cap1.png"
+import cap6 from "../assets/certs/cap6.png"
 import cap2 from "../assets/certs/cap2.png"
 import cap3 from "../assets/certs/cap3.png"
 import cap4 from "../assets/certs/cap4.jpg"
@@ -18,6 +19,7 @@ export default function Certs(){
             <p className="font-light text-gray-400">Voci quelques projets</p>
             <div className="grid grid-cols-1 md:grid-cols-3 justify-center mt-4 gap-5">
                 <CertCard name="Planete web un projet statique en HTML et CSS" img={cap1} date="Juin 2023" />
+                <CertCard name="CarSales un site vitrine pour un vendeur automobile (fictif), on n'y trouve du HTML, du CSS et du Javascript" img={cap6} date="Décembre 2023" />
                 <CertCard name="Wolf-Gang, intégration en HTML et CSS" img={cap2} date="Juillet 2023" />
                 <CertCard name="Devlove, Une BDD pour un site de rencontre (fictif) réaliser sur MySql Workbench" img={cap3} date="Septembre 2023" />
                 <CertCard name="Chat-Box, Une petite messagerie en PHP afin de m'exercer d'avantage sur les SESSIONS" img={cap4} date="Octobre 2023" />
