0572779877645c7246cb4fc908d222c8fe4bad62
eroudini <eroudini.abdillah@yahoo.com>
2023-12-14 15:44:12 +0100

    nc

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,7 +16,7 @@ import reactIcon from "../assets/skills/react.svg"
 import git from "../assets/skills/git.svg"
 import figma from "../assets/skills/figma.svg"
 import vue from "../assets/skills/vue.svg"
-import wordpress from "../assets/skills/wordpress.svg"
+
 
 import hr from "../assets/curve-hr.svg"
 
@@ -50,7 +50,6 @@ export default function Skills() {
                 <SkillCard name="Git"  img={git} />
                 <SkillCard name="Figma" img={figma} />
                 <SkillCard name="Vue" img={vue} />
-                <SkillCard name="Wordpress" img={wordpress} />
                 </Slider>
             </div>
             <img src={hr} className="w-full mt-8 md:h-3" alt="hr" />
